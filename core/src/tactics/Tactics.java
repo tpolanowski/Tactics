bcb4e164c0b04ccdb112626ef37be6151b513304
tpolanowski <polanowski.tomasz@gmail.com>
2015-08-23 23:00:56 +0200

    Tilemap init

--- core/src/tactics/Tactics.java
+++ core/src/tactics/Tactics.java
@@ -2,26 +2,160 @@ package tactics;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class Tactics extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Tactics extends ApplicationAdapter implements InputProcessor{
+	//SpriteBatch batch;
+	//Texture img;
+
+	TiledMap tiledMap;
+	TiledMapRenderer tiledMapRenderer;
+	OrthographicCamera camera;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//batch = new SpriteBatch();
+		//img = new Texture("badlogic.jpg");
+
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("test.tmx");
+		tiledMapRenderer = new IsometricTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		//batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
+
+		Gdx.gl.glClearColor(0, 0.1f, 0.05f, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_CONSTANT_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+	}
+
+	/**
+	 * Called when a key was pressed
+	 *
+	 * @param keycode one of the constants in {@link com.badlogic.gdx.Input.Keys}
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	/**
+	 * Called when a key was released
+	 *
+	 * @param keycode one of the constants in {@link com.badlogic.gdx.Input.Keys}
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-32,0);
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(32,0);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0,-32);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0,32);
+		if(keycode == Input.Keys.NUM_1)
+			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+		if(keycode == Input.Keys.NUM_2)
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+		return false;
+	}
+
+	/**
+	 * Called when a key was typed
+	 *
+	 * @param character The character
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	/**
+	 * Called when the screen was touched or a mouse button was pressed. The button parameter will be {@link Buttons#LEFT} on iOS.
+	 *
+	 * @param screenX The x coordinate, origin is in the upper left corner
+	 * @param screenY The y coordinate, origin is in the upper left corner
+	 * @param pointer the pointer for the event.
+	 * @param button  the button
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	/**
+	 * Called when a finger was lifted or a mouse button was released. The button parameter will be {@link Buttons#LEFT} on iOS.
+	 *
+	 * @param screenX
+	 * @param screenY
+	 * @param pointer the pointer for the event.
+	 * @param button  the button   @return whether the input was processed
+	 */
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	/**
+	 * Called when a finger or the mouse was dragged.
+	 *
+	 * @param screenX
+	 * @param screenY
+	 * @param pointer the pointer for the event.  @return whether the input was processed
+	 */
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	/**
+	 * Called when the mouse was moved without any buttons being pressed. Will not be called on iOS.
+	 *
+	 * @param screenX
+	 * @param screenY
+	 * @return whether the input was processed
+	 */
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	/**
+	 * Called when the mouse wheel was scrolled. Will not be called on iOS.
+	 *
+	 * @param amount the scroll amount, -1 or 1 depending on the direction the wheel was scrolled.
+	 * @return whether the input was processed.
+	 */
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
