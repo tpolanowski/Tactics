370afe7d731c8014a961d904b4fed7fd2f059cfb
tpolanowski <polanowski.tomasz@gmail.com>
2015-08-23 19:49:28 +0200

    Initial commit

---
+++ desktop/src/tactics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tactics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tactics.Tactics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tactics(), config);
+	}
+}
